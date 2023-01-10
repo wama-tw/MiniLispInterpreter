a05be1ae12cc43e7fe0259aa26c2764b9fc5ae1a
wama <36957874+april1026@users.noreply.github.com>
2023-01-10 13:16:22 +0800

    feat: add bouns feature test options

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,31 @@
+DarkGray='\033[1;30m'
+Yellow='\033[1;33m'
+NC='\033[0m' # No Color
 for i in $(seq $1 $2)
 do
-    # echo "$i"
     for j in $(seq $3 $4)
     do
-        # echo "$j"
         echo ===============================
-        echo public_test_data/0"$i"_"$j".lsp
+        echo "${DarkGray}"public_test_data/0"$i"_"$j".lsp"${NC}"
         cat ../public_test_data/0"$i"_"$j".lsp
-        echo 
+        echo "${Yellow}"
         python3 yacc.py ../public_test_data/0"$i"_"$j".lsp
+        echo "${NC}"
     done
 done
+
+if [ $# -eq 5 ]
+then
+    for i in $(seq 1 $5)
+    do
+        for j in $(seq 1 2)
+        do
+            echo ===============================
+            echo "${DarkGray}"public_test_data/b"$i"_"$j".lsp"${NC}"
+            cat ../public_test_data/b"$i"_"$j".lsp
+            echo "${Yellow}"
+            python3 yacc.py ../public_test_data/b"$i"_"$j".lsp
+            echo "${NC}"
+        done
+    done
+fi
